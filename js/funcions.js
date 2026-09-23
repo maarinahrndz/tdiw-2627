603f6d1d079cb88667a99c8ec50048ef64951115
maarinahrndz <1750251@uab.cat>
2026-09-23 13:06:15 +0200

    p2 principi

--- js/funcions.js
+++ js/funcions.js
@@ -1,3 +1,7 @@
 function confirmaRegistre(){
     //completa
+    alert("Registering user")
+    console.log("Registering user")
+    document.getElementById("formDiv").innerHTML = "<p class=\"important\">Registered successfully</p>"
+
 }
